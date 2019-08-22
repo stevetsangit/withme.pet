31e68022f043843d01b788adf140577b17adb828
Steve Tsang <stevagesz@gmail.com>
2019-08-22 12:14:43 +0800

    qrwin

---
+++ script.js
@@ -0,0 +1,16 @@
+let bodyEl = document.body
+let top = 0
+
+function stopBodyScroll (isFixed) {
+  if (isFixed) {
+    top = window.scrollY
+
+    bodyEl.style.position = 'fixed'
+    bodyEl.style.top = -top + 'px'
+  } else {
+    bodyEl.style.position = ''
+    bodyEl.style.top = ''
+
+    window.scrollTo(0, top) // 回到原先的top
+  }
+}
